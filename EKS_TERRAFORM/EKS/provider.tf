9a4bcfea6e52909c5dafdfab62ef2a58fa16033d
aikdp <kdprasad028@gmail.com>
2025-05-25 13:54:55 +0530

    Clahan fss repo

--- EKS_TERRAFORM/EKS/provider.tf
+++ EKS_TERRAFORM/EKS/provider.tf
@@ -6,11 +6,7 @@ terraform {
     }
   }
   backend "s3" { #need to create manually or use tf in AWS, then use here
-<<<<<<< HEAD
-    bucket         = "fss-fss-Retail-App_kubernetes-project" 
-=======
     bucket         = "fss-fss-retail-app-kubernetes-project" 
->>>>>>> 04bf784 (Clahan fss repo)
     key            = "eks-clahan.tfstate"
     region         = "us-east-1"
     # dynamodb_table = "vpc-module-locking"
